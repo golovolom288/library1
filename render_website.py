f82e707cf38d42523378053d0422d3e6cca1d298
golovolom288 <142310490+golovolom288@users.noreply.github.com>
2025-06-13 20:26:27 +0600

    ntegrate livereload.

--- render_website.py
+++ render_website.py
@@ -2,21 +2,25 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import json
 from flask import render_template
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html'])
-)
-template = env.get_template('template.html')
-
-with open("media/meta_data.json", "r+", encoding="utf-8") as json_file:
-    books = json.load(json_file)
-
-render_page = template.render(books=books)
-
-with open("index.html", "w+", encoding="utf-8") as template:
-    template.write(render_page)
+from livereload import Server, shell
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def render_site():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
+    )
+    template = env.get_template('template.html')
+
+    with open("media/meta_data.json", "r+", encoding="utf-8") as json_file:
+        books = json.load(json_file)
+
+    render_page = template.render(books=books)
+
+    with open("index.html", "w+", encoding="utf-8") as template:
+        template.write(render_page)
+
+
+server = Server()
+server.watch('template.html', render_site)
+server.serve()

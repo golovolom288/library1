3b39a9642cafedb088251ceb77f98975a846351d
golovolom288 <142310490+golovolom288@users.noreply.github.com>
2025-06-20 20:47:22 +0600

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,8 +1,8 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 import json
-from flask import render_template
+import os
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from livereload import Server, shell
+from livereload import Server
 from more_itertools import chunked
 
 
@@ -11,18 +11,21 @@ def render_site():
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
-    template = env.get_template('template.html')
 
     with open("media/meta_data.json", "r+", encoding="utf-8") as json_file:
         books = json.load(json_file)
+    books = list(chunked(books, 20))
+    os.makedirs("pages", exist_ok=True)
+    for i, twenty_books in enumerate(books, 1):
+        template = env.get_template('template.html')
+        render_page = template.render(books=list(chunked(twenty_books, 2)))
 
-    render_page = template.render(books=list(chunked(books, 2)))
-
-    with open("index.html", "w+", encoding="utf-8") as template:
-        template.write(render_page)
+        with open(f"pages/index{i}.html", "w+", encoding="utf-8") as template:
+            template.write(render_page)
+            template.close()
 
 
 render_site()
 server = Server()
 server.watch('template.html', render_site)
-server.serve()
+server.serve(default_filename="pages/index1.html")

a282b31b1b168651af48f93a71059485e8dc0803
golovolom288 <142310490+golovolom288@users.noreply.github.com>
2025-07-06 17:22:50 +0600

    доделал правки

--- render_website.py
+++ render_website.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import urllib.parse
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -8,35 +9,39 @@ from more_itertools import chunked
 from dotenv import load_dotenv
 
 
-def render_site():
-    load_dotenv()
+def render_site(json_file_path):
+    with open(json_file_path, "r", encoding="utf-8") as json_file:
+        books = json.load(json_file)
     env = Environment(
         loader=FileSystemLoader("."),
         autoescape=select_autoescape(["html"])
     )
     count_books_on_page = 20
+    books_page = list(chunked(books, count_books_on_page))
+    os.makedirs("pages", exist_ok=True)
+    for page_id, page_with_books in enumerate(books_page, start=1):
+        template = env.get_template("template.html")
+        render_page = template.render(
+            books=page_with_books,
+            pages_count=len(books_page),
+            selected_page=page_id
+        )
+        with open(f"pages/index{page_id}.html", "w", encoding="utf-8") as template:
+            template.write(render_page)
+            template.close()
+
+
+def main():
+    load_dotenv()
     parser = argparse.ArgumentParser(
         prog='Online library',
         description='Online library for reading',
     )
     parser.add_argument("data_filepath", nargs="?", default=os.getenv("DEFAULT_DATA_FILEPATH"))
     args = parser.parse_args()
-    with open(args.data_filepath, "r+", encoding="utf-8") as json_file:
-        books = json.load(json_file)
-    books = list(chunked(books, count_books_on_page))
-    os.makedirs("pages", exist_ok=True)
-    for page_id, page_with_books in enumerate(books, 1):
-        template = env.get_template("template.html")
-        render_page = template.render(books=list(chunked(page_with_books, 2)), pages_count=len(books), selected_page=page_id)
-        with open(f"pages/index{page_id}.html", "w+", encoding="utf-8") as template:
-            template.write(render_page)
-            template.close()
-
-
-def main():
-    render_site()
+    render_site(args.data_filepath)
     server = Server()
-    server.watch("template.html", render_site)
+    server.watch("template.html", lambda: render_site(args.data_filepath))
     server.serve(default_filename="pages/index1.html", root=".")
 
 

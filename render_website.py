92aaa61e800ab00b17891d543f6f6d25f37e1d36
golovolom288 <142310490+golovolom288@users.noreply.github.com>
2025-07-06 15:39:00 +0600

    объяснил переменные

--- render_website.py
+++ render_website.py
@@ -11,14 +11,14 @@ def render_site():
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
-
+    count_books_on_page = 20
     with open("media/meta_data.json", "r+", encoding="utf-8") as json_file:
         books = json.load(json_file)
-    books = list(chunked(books, 20))
+    books = list(chunked(books, count_books_on_page))
     os.makedirs("pages", exist_ok=True)
-    for i, twenty_books in enumerate(books, 1):
+    for i, page_with_books in enumerate(books, 1):
         template = env.get_template('template.html')
-        render_page = template.render(books=list(chunked(twenty_books, 2)), pages_count=len(books), selected_page=i)
+        render_page = template.render(books=list(chunked(page_with_books, 2)), pages_count=len(books), selected_page=i)
         with open(f"pages/index{i}.html", "w+", encoding="utf-8") as template:
             template.write(render_page)
             template.close()
